3ce0a95872d1d7be3114cf38d503fa7fb068db0a
turgunbek <omurkanov@gmail.com>
2023-09-27 07:42:34 +0300

    Deleted unnessesary file

--- task_9_winds.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0').json()
-dataseries = response['dataseries']
-for i in range(len(dataseries)):
-    direction = dataseries[i]['wind10m']['direction']
-    speed = dataseries[i]['wind10m']['speed']
-    print(f'направление: {direction}, скорость: {speed}')

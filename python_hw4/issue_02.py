83a56450b574bba3d6664f9ee4db0f4bad4c7651
turgunbek <omurkanov@gmail.com>
2023-11-01 11:55:11 +0300

    Созданы файлы в соответствии с требованиями домашнего задания по теме "Тестирование". Файлы с тестами и функциями названы "issue_0*.py", результаты запусков команд названы соответственно "result issue_0*.txt"

---
+++ python_hw4/issue_02.py
@@ -0,0 +1,59 @@
+import pytest
+
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def decode(morse_message: str) -> str:
+    """
+    Декодирует строку из азбуки Морзе в английский
+    """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+@pytest.mark.parametrize(
+    'morse_message, expected, expects_exceptions', [
+        ('.-', 'A', False),
+        ('.... . .-.. .-.. ---', 'HELLO', False),
+        ('... --- ...', 'SOS', False),
+        ('', '', False),
+        (' ', ' ', True)
+    ]
+)
+def test_decode(morse_message, expected, expects_exceptions):
+    if expects_exceptions:
+        with pytest.raises(AssertionError):
+            result = decode(morse_message)
+            assert result == expected
+    else:
+        result = decode(morse_message)
+        assert result == expected
+
+
+if __name__ == '__main__':
+    pass
